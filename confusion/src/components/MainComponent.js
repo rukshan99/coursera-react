b51bd9ba012cf19e116fb50a9668e9f05cfafe3c
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-10 12:53:41 +0530

    update main component

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -24,15 +24,6 @@ class Main extends Component {
     };
   }
 
-  DishWithId = (match) => {
-    return(
-      <DishDetail
-        dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-      />
-    );
-  }
-
   render() {
     const HomePage = () => {
       return (
@@ -40,6 +31,14 @@ class Main extends Component {
           dish={this.state.dishes.filter((dish) => dish.featured)[0]}
           leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+        />
+      );
+    }
+    const DishWithId = (match) => {
+      return (
+        <DishDetail
+          dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
         />
       );
     }
