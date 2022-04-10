21e268abf3fbb6f53cd429dd03371cae88228f2f
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-10 11:41:33 +0530

    update home component

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -7,6 +7,9 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
@@ -14,13 +17,20 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+        />
       );
     }
     return (
