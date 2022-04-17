b43ee2e39cbfc3c2c2a43a20520fddc03b166c39
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-17 19:31:24 +0530

    bug fix

--- confusion/src/redux/dishes.js
+++ confusion/src/redux/dishes.js
@@ -1,6 +1,6 @@
 import { DISHES } from '../shared/dishes';
 
-export const Dishes = (state = Dishes, action) => {
+export const Dishes = (state = DISHES, action) => {
     switch(action.type) {
         default:
             return state;
