eb36afd81d6472fc9c8bc7900979be3260deb531
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-17 19:16:57 +0530

    dishes reducer: initial structure

---
+++ confusion/src/redux/dishes.js
@@ -0,0 +1,8 @@
+import { DISHES } from '../shared/dishes';
+
+export const Dishes = (state = Dishes, action) => {
+    switch(Selection.type) {
+        default:
+            return state;
+    }
+}
