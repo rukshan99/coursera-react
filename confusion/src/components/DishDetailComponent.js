06e3a65856698433c08408faf1f260a6e7783c2f
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-20 19:17:50 +0530

    update image links with +baseUrl and update renderCardfor promotions

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -89,7 +90,7 @@ class CommentForm extends Component {
 const renderDish = (dish) => {
     return (
         <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } 
 import { Link } from 'react-router-dom';
 
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const Menu = (props) => {
     const menu = props.dishes.dishes.map((dish) => {
@@ -10,7 +11,7 @@ const Menu = (props) => {
             <div className="col-12 col-md-5 m-1">
                 <Card key={dish.id}>
                     <Link to={`/menu/${dish.id}`} >
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
