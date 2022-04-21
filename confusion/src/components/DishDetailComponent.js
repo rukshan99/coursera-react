375de52fecc6a7002bef0268cab5048d4d367566
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2022-04-21 21:53:09 +0530

    add animations with react-animation-components

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -6,6 +6,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
@@ -89,25 +90,35 @@ class CommentForm extends Component {
 
 const renderDish = (dish) => {
     return (
-        <Card>
-            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
 const renderComments = (comments) => {
-    const commentList = comments.map((comment, i) => {
-        return (
-            <li key={i}>
-                {comment.comment} <br />
-                --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-            </li>
-        );
-    });
+    const commentList = <Stagger in>
+        {comments.map((comment) => {
+            return (
+                <Fade in>
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                    </li>
+                </Fade>
+            );
+        })}
+    </Stagger>
     return (
         <React.Fragment>
             <h4>Comments</h4>
